6365f2ff55a599af11238ed6e3b15e33f14701ea
Vasco Campos <1160678@isep.ipp.pt>
2019-11-01 18:36:49 +0000

    pré-processamento da população e teste da distribuição

--- projeto_VD_EDA_vasco.R
+++ projeto_VD_EDA_vasco.R
@@ -14,9 +14,9 @@ nrow(crimes_2001_2004)
 #Funções -----------------------------
 
 libraries <- function() {
-  require(data.table) #fread
-  library(readr) #read_csv
+  require(data.table) #fread / apply
   library(rstudioapi)
+  library(tidyverse) #readr / dplyr / ggplot2
 }
 
 read_files <- function() {
@@ -43,7 +43,7 @@ crimes_2012_2017 = files_list$`2012_2017`
 
 
 
-#pré processamento -------------------
+#PRÉ-PROCESSAMENTO -------------------
 
 #retirar coluna a mais
 crimes_2001_2004 = crimes_2001_2004[, !(names(crimes_2001_2004) %in% "X1")]
@@ -51,6 +51,7 @@ crimes_2005_2007 = crimes_2005_2007[, !(names(crimes_2005_2007) %in% "X1")]
 crimes_2008_2011 = crimes_2008_2011[, !(names(crimes_2008_2011) %in% "X1")]
 crimes_2012_2017 = crimes_2012_2017[, !(names(crimes_2012_2017) %in% "X1")]
 View(crimes_2008_2011)
+View(crimes_2001_2004)
 #30692 NA na latitude, longitude, location, x coordinate e y coordinate
 #700248 NA na ward e Community area
 
@@ -68,6 +69,82 @@ registerDoParallel(cores = 4)
 #4. comparar as percentagens de cada tipo de modo a perceber se a distribuição não se alterou muito
 
 
+#Verificar se os thefts englobam os restantes tipos de roubo:
+#Resposta: não, cada roubo é especifico, o theft é usado mais para roubo de dinheiro (ou roubo em geral)
+#Robbery: Ameaça de uso de força com intenção de roubar
+#Assault: Uso de força ou ofensa à integridade fisica de uma pessoa com intenção de roubar
+#Burglary: Entrada forçada ou ilegal em propriedade privada ou alheia com intenção de roubar
+crimes_sample = crimes_2008_2011[-c(1:2, 5, 9:22)]
+colnames(crimes_sample)[3] = "Primary_Type"
+View(crimes_sample)
 
-#https://www.researchgate.net/post/How_to_sample_a_smaller_data_set_from_a_very_large_data_set
-#https://analyticsindiamag.com/data-preprocessing-with-r-hands-on-tutorial/
+sample_thefts = filter(crimes_sample, Primary_Type %in% c("THEFT", "ROBBERY", "ASSAULT", "BURGLARY"))
+View(sample_thefts)
+
+sample_thefts = filter(crimes_sample, Primary_Type %in% "OFFENSE INVOLVING CHILDREN")
+View(sample_thefts)
+
+
+
+
+
+
+
+#SAMPLING DO DATASET-------------
+#Eliminar colunas não necessárias
+non_sampled_2001_2004 = crimes_2001_2004[-c(1:2, 4:5, 11, 13:17, 19:22)]
+
+colnames(non_sampled_2001_2004)[2] = "Primary_Type"; colnames(non_sampled_2001_2004)[4] = "Location_Description"
+View(non_sampled_2001_2004)
+
+#Tipos de crime com percentagem 0:
+#DOMESTIC VIOLENCE / IUCR (erro no df) / NON-CRIMINAL / OBSCENITY / OTHER NARCOTIC VIOLATION / PUBLIC INDECENCY / RITUALISM
+non_sampled_2001_2004 = filter(non_sampled_2001_2004, !Primary_Type %in% c("DOMESTIC VIOLENCE", "IUCR", "NON-CRIMINAL", "OBSCENITY", "OTHER NARCOTIC VIOLATION", "PUBLIC INDECENCY", "RITUALISM"))
+
+#Tabela de frequência do Primary_Type e Description
+sample_2001_2004_table = lapply(non_sampled_2001_2004["Primary_Type"], table)
+
+crime_types = as.data.frame(sample_2001_2004_table$Primary_Type)
+colnames(crime_types)[1] = "Type"
+
+#options("scipen"=100, "digits"=4) #não mostrar potencias de 10
+#percentagem de ocorrencia de cada crime
+crime_types = crime_types %>% 
+  mutate(Type, percent = round((Freq / nrow(non_sampled_2001_2004)) * 100, 2))
+
+View(crime_types)
+
+
+
+#sampling de 25%
+nrow(non_sampled_2001_2004)
+sample_2001_2004 = non_sampled_2001_2004[seq(1, nrow(non_sampled_2001_2004), 4), ]
+nrow(sample_2001_2004)
+
+
+#Verificação de percentagens
+sample_2001_2004_table = lapply(sample_2001_2004["Primary_Type"], table)
+
+sampled_crime_types = as.data.frame(sample_2001_2004_table)
+colnames(sampled_crime_types) = c("Type", "Freq")
+
+sampled_crime_types = sampled_crime_types %>% 
+  mutate(Type, percent = round((Freq / nrow(sample_2001_2004)) * 100, 2))
+
+View(sampled_crime_types)
+
+
+
+
+#Transformar valores categóricos de Primary_Type em valores numericos
+numeric_primary_type_pop1 = unclass(as.factor(non_sampled_2001_2004$Primary_Type))
+numeric_primary_type_pop2 = unclass(as.factor(sample_2001_2004$Primary_Type))
+
+mean(numeric_primary_type_pop1)
+mean(numeric_primary_type_pop2)
+
+median(numeric_primary_type_pop1)
+median(numeric_primary_type_pop2)
+
+sd(numeric_primary_type_pop1)
+sd(numeric_primary_type_pop2)
